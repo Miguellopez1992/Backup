3d89b7e1306563d6eb2b1d68cde3e223b6043903
Daniela <Daniela@Daniela>
2014-06-02 22:45:41 -0430

    author corrected

--- src/backup/controller/ControllerRW.java
+++ src/backup/controller/ControllerRW.java
@@ -6,12 +6,19 @@
 
 package backup.controller;
 
-import com.sun.corba.se.impl.io.IIOPOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import backup.interfaces.RWSetting;
 import backup.model.Setting;
+import com.sun.corba.se.impl.io.IIOPOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,8 +27,10 @@ import backup.model.Setting;
 public class ControllerRW implements RWSetting<Setting>{
     
     private static ControllerRW controllerRW;
+    private  File file;
     
     private ControllerRW() {
+        file=new File(System.getProperty("user.dir")+"/setting.set");
     }
 
     /**
@@ -43,9 +52,18 @@ public class ControllerRW implements RWSetting<Setting>{
      */
     @Override
     public boolean save(Setting obj) {
-//        OutputStream out=new 
-////        ObjectOutputStream output=new ObjectOutputStream(null);
-        
+        createFile();
+        ObjectOutputStream output;
+        try {
+            output=new ObjectOutputStream(new FileOutputStream(file));
+            output.flush();
+            output.writeObject(obj);
+            output.close();
+        } catch (FileNotFoundException ex) {
+            //Logger
+        } catch (IOException  ex) {
+            //Logger
+        }
         return true;
     }
 
@@ -55,7 +73,29 @@ public class ControllerRW implements RWSetting<Setting>{
      */
     @Override
     public Setting getSetting() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ObjectInputStream input;
+        Setting s;
+        try {
+            input = new ObjectInputStream(new FileInputStream(file));
+            s=(Setting) input.readObject();
+            input.close();
+            return s;
+        } catch (IOException | ClassNotFoundException ex) {
+            //Logger
+        }
+        return null;
+    }
+
+    private boolean createFile() {
+        if(!file.exists()){
+            try {
+                return file.createNewFile();
+            } catch (IOException ex) {
+               //Logger
+               return false;
+            }
+        }
+        return true;
     }
     
     
